50f0e10541861e966cdeeb60b2b26ae64d004daf
John Kipruto <johnkipruto157@gmail.com>
2024-03-12 10:41:23 +0300

    Justify content to space between for cart item

--- client/src/components/chekout/cart_summary/cart_item/CartItem.js
+++ client/src/components/chekout/cart_summary/cart_item/CartItem.js
@@ -3,11 +3,11 @@ import './cart_item.css'
 const CartItem = ({item}) => {
   return (
     <div className='cart-item flex-row-center'>
-        <div className='flex-row-center'>
+        <div >
             <div>{item.name.slice(0, 15)}</div>
             <div>${item.price}</div>
         </div>
-        <div className='flex-row-center'>
+        <div >
             <div>Qty: {item.quantity}</div>
             <div>Tot: <b>${item.price * item.quantity}</b></div>
         </div>
